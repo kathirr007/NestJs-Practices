919b2f66ef550a534a572b1fa0045432af48c12d
Kathirr007 <kathirr007@gmail.com>
2025-12-25 16:05:34 +0530

    Added basic coffees module with all neccessary parts like controller, service, dto and entities

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CoffeesService } from 'src/coffees/coffees.service';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+import { UpdateCoffeeDto } from 'src/coffees/dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
+  @Get()
+  findAll(@Query() paginationQuery: any) {
+    // const { limit, offset } = paginationQuery;
+    // return `This action returns all coffees with limit ${limit} and offset of ${offset}`;
+    return this.coffeesService.findAll();
+  }
+
+  @Get(':id')
+  //   findOne(@Param() params) {
+  findOne(@Param('id') id: string) {
+    // return `This action returns a #${id} coffee`;
+    return this.coffeesService.findOne(id);
+  }
+
+  @Post()
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    // return body;
+    return this.coffeesService.create(createCoffeeDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    // return `This update the coffee with id ${id} with the following body ${body}`;
+    return this.coffeesService.update(id, updateCoffeeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    // return `This removes the coffee with id ${id}`;
+    return this.coffeesService.delete(id);
+  }
+}
---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,70 @@
+import { Body, Injectable, NotFoundException } from '@nestjs/common';
+import { CreateCoffeeDto } from 'src/coffees/dto/create-coffee.dto';
+import { Coffee } from 'src/coffees/entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Coffee no.1',
+      flavors: ['chocolate', 'vanilla'],
+      brand: 'Coffee brand 01',
+    },
+    {
+      id: 2,
+      name: 'Coffee no.2',
+      flavors: ['caramel', 'hazelnut'],
+      brand: 'Coffee brand 02',
+    },
+    {
+      id: 3,
+      name: 'Coffee no.3',
+      flavors: ['mocha', 'almond'],
+      brand: 'Coffee brand 03',
+    },
+    {
+      id: 4,
+      name: 'Coffee no.4',
+      flavors: ['toffee', 'cinnamon'],
+      brand: 'Coffee brand 04',
+    },
+    {
+      id: 5,
+      name: 'Coffee no.5',
+      flavors: ['irish cream', 'coconut'],
+      brand: 'Coffee brand 05',
+    },
+  ];
+
+  findAll(): Coffee[] {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+    if (!coffee) {
+      throw new NotFoundException(`Coffee with id ${id} not found`);
+    }
+
+    return coffee;
+  }
+
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // Update the existing coffee
+    }
+  }
+
+  delete(id: string) {
+    const coffeeIndex = this.coffees.findIndex((coffee) => coffee.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
