94e65f90740fb28528e906b85d93d6eb511c090f
Kathirr007 <kathirr007@gmail.com>
2025-12-26 01:36:36 +0530

    updated docker-compose and removed unwanted files

--- init.sql
+++
@@ -1,8 +0,0 @@
--- Create app database and user
-CREATE USER nestapp WITH PASSWORD 'nestapp123';
-CREATE DATABASE nestapp_db OWNER nestapp;
-GRANT ALL PRIVILEGES ON DATABASE nestapp_db TO nestapp;
-
--- Allow connections
-\c nestapp_db
-GRANT ALL ON SCHEMA public TO nestapp;
