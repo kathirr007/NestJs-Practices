7e134de1fe9343855d783534c591e16639d708c5
Kathirr007 <kathirr007@gmail.com>
2025-12-25 14:11:34 +0530

    updated main bootstrap function to show the url in the log

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+  await app
+    .listen(process.env.PORT ?? 3000)
+    .then(() =>
+      console.log(
+        `Server listening on - http://localhost:${process.env.PORT || 3000}`,
+      ),
+    )
+    .catch((err) => console.log(err));
 }
+
 bootstrap();
